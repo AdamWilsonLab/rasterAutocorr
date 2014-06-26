4fbbf0ba93183a2e26aa4c5cae67d791a2fb2ca2
Adam M. Wilson <adammichaelwilson@gmail.com>
2014-06-26 14:22:46 -0400

    updating to use raster projection for accurate distance calculation

--- R/acorr2.R
+++ R/acorr2.R
@@ -17,10 +17,7 @@
 #x=m1
 
 acorr2=function(x,gain=NULL,...){
-  ## convert to matrix
-  x1=as(x,"matrix")
-  ## pad the data
-  # dimensions of data matrix
+  # dimensions of raster
   nr <- nrow(x)
   nc <- ncol(x)
   ## Images must be padded to size 2N-1 by 2M-1
@@ -28,19 +25,28 @@ acorr2=function(x,gain=NULL,...){
   # speed (a power of 2) and memory required
   nr2=ceiling((2*nr-1)/8)*8
   nc2=ceiling((2*nc-1)/8)*8  
-  x2=matrix(0,nrow=nr2,ncol=nc2)
-  x2[1:nr,1:nc]=x1
+  ## pad the matrix
+  ## create a new extent
+  resx=res(x)
+  extx=extent(x)
+  extx2=extent(c(xmin=extx@xmin,xmax=extx@xmax+(resx[1]*(nc2-nc)),ymin=extx@ymin-(resx[2]*(nr2-nr)),ymax=extx@ymax))
+  rx=extend(x,extx2,val=0)
+  ## convert to matrix
+  x1=as(rx,"matrix")
+  ## pad the data
+#  x2=matrix(0,nrow=nr2,ncol=nc2)
+#  x2[1:nr,1:nc]=x1
 
   # form an indicator matrix:
   # 1's for all data values
   # O's for missing values
   # in data matrix, replace missing values by 0;
   xnull=matrix(0,nrow=nr2,ncol=nc2)
-  xnull[1:nr,1:nc][!is.na(x1)]=1
-  x2[xnull==0]=0
+  xnull[1:nr,1:nc][as.matrix(!is.na(x))]=1
+  x1[xnull==0]=0
 
-  fx1=fft(x2)  # fourier transform of xl
-  fx1_x1=fft(x2*x2)    # fourier transform of x1*x1
+  fx1=fft(x1)  # fourier transform of xl
+  fx1_x1=fft(x1*x1)    # fourier transform of x1*x1
   
   fxnull=fft(xnull)  # fourier transform of the indicator matriJ
   # compute number of pairs at all lags
@@ -53,8 +59,8 @@ acorr2=function(x,gain=NULL,...){
   #rm(fx1_x1,fxnull,fx1)     ## clean up
 
   ## shift the matrix to bring the low frequency autocorrelation to the center of the image
-  g2=fftshift2(g)
-  nobs2=fftshift2(nobs)
+#  g2=fftshift2(g)
+#  nobs2=fftshift2(nobs)
 #  rm(g,nobs);gc()
   ## crop to original dimensions
   nre=ifelse(nrow(g2)/2==round(nrow(g2)/2),1,2)
@@ -65,7 +71,6 @@ nobs2=rbind(
   cbind(nobs[(nr2-nr+2):nr2,1:nc],nobs[(nr2-nr+2):nr2,(nc2-nc+2):nc2])
 )
 nobs3=fftshift2(nobs2)      
-
 g2=rbind(
   cbind(g[1:nr,1:nc],          g[1:nr,(nc2-nc+2):nc2]),
   cbind(g[(nr2-nr+2):nr2,1:nc],g[(nr2-nr+2):nr2,(nc2-nc+2):nc2])
@@ -76,11 +81,11 @@ g3=fftshift2(g2)
 #nobs3=nobs2[(nre+(nr2-nr)/2):(nr+((nr2-nr)/2)),(nce+(nc2-nc)/2):(nc+(nc2-nc)/2)]
 
 ## get distances in km
-d1=acorr_dist(nobs3,nc=nc,nr=nr)
+d1=acorr_dist(rx,nc=nc,nr=nr)
 
 # convert to raster
-  g4=raster(g3,xmn=-nc/2,xmx=nc/2,ymn=-nr/2,ymx=nr/2)
-  nobs4=raster(nobs3,xmn=-nc/2,xmx=nc/2,ymn=-nr/2,ymx=nr/2)
+  g4=d1;values(g4)=g3*10
+  nobs4=d1;values(nobs4)=nobs3
   acor=stack(g4,nobs4,d1)
   names(acor)=c("acor","nobs","dist")
   if(exists("filename",inherits=F)) acor2=writeRaster(,...)
