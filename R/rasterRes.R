c55d0c389ddf876bd4babeedd0fd036bcd35d245
Adam M. Wilson <adammichaelwilson@gmail.com>
2014-09-12 12:40:54 -0400

    rasterres

--- R/rasterRes.R
+++ R/rasterRes.R
@@ -4,7 +4,7 @@ rasterRes=function(x){
 ##  extract middle portion of raster
   x2=x[(nr/2-1):(nr/2+1),(nc/2-1):(nc/2+1),drop=F]
 ## get distance matrix in km
-  d1=as.matrix(acorr_dist(x2))
+  d1=as(acorr_dist(x2),"matrix")
 ## get mean in x & y directions
   return(mean(c(d1[2,1],d1[1,2],d1[3,2],d1[2,3])))
 }
